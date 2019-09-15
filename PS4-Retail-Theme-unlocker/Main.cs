effa540c9d64dced93788f7bacaa0a0197ade0b0
Backporter <29647778+Backporter@users.noreply.github.com>
2019-09-14 23:11:19 -0400

    v1.1
    Updated Souce code

--- PS4-Retail-Theme-unlocker/Main.cs
+++ PS4-Retail-Theme-unlocker/Main.cs
@@ -20,6 +20,7 @@ namespace PS4_Retail_Theme_unlocker
         {
             InitializeComponent();
         }
+
         private void Main_Load(object sender, EventArgs e)
         {
             /// thse are what you see when you hover over the buttons
@@ -147,12 +148,54 @@ namespace PS4_Retail_Theme_unlocker
             {
                 if (filePaths[i].Contains(".pkg"))
                 {
+                    string CUSA = Path.GetFileNameWithoutExtension(filePaths[i]).Substring(7, 9);
+                    string EPID = Path.GetFileNameWithoutExtension(filePaths[i]);
+                    Directory.CreateDirectory("fake_dlc_pkg");
+                    Directory.CreateDirectory("fake_dlc_temp");
+                    Directory.CreateDirectory("fake_dlc_temp\\sce_sys");
+                    if (File.Exists("fake_dlc_temp\\param_template.sfx") == true)
+                    {
+                        File.Delete("fake_dlc_temp\\param_template.sfx");
+                    }
+                    File.Copy("Data\\param_template.sfx", "fake_dlc_temp\\param_template.sfx");
+                    string text = File.ReadAllText("fake_dlc_temp\\param_template.sfx");
+                    string exportinfo = File.ReadAllText("fake_dlc_temp\\param_template.sfx");
+                    text = text.Replace("%1", EPID);
+                    exportinfo = text.Replace("%2", CUSA);
+                    File.WriteAllText("fake_dlc_temp\\param_template.sfx", text);
+                    File.WriteAllText("fake_dlc_temp\\param_template.sfx", exportinfo);
                     Process process1 = new Process();
                     process1.StartInfo.FileName = "cmd.exe";
                     process1.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    process1.StartInfo.Arguments = "/c ez_fake_dlc.py \"" + Path.GetFileNameWithoutExtension(filePaths[i]) + "\" \"" + "\"theme\"";
+                    process1.StartInfo.Arguments = "/c data\\orbis-pub-cmd.exe sfo_create \"" + "fake_dlc_temp\\param_template.sfx" + "\" \"" + "fake_dlc_temp\\sce_sys\\param.sfo";
                     process1.Start();
                     process1.WaitForExit();
+                    if (File.Exists("fake_dlc_temp\\fake_dlc_project.gp4") == true)
+                    {
+                        File.Delete("fake_dlc_temp\\fake_dlc_project.gp4");
+                    }
+                    File.Copy("Data\\fake_dlc_project.gp4", "fake_dlc_temp\\fake_dlc_project.gp4");
+                    string time = File.ReadAllText("fake_dlc_temp\\fake_dlc_project.gp4");
+                    time = time.Replace("TIME", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                    File.WriteAllText("fake_dlc_temp\\fake_dlc_project.gp4", time);
+                    string id = File.ReadAllText("fake_dlc_temp\\fake_dlc_project.gp4");
+                    id = id.Replace("ID", EPID);
+                    File.WriteAllText("fake_dlc_temp\\fake_dlc_project.gp4", id);
+                    string dir = File.ReadAllText("fake_dlc_temp\\fake_dlc_project.gp4");
+                    dir = dir.Replace("DIR", AppDomain.CurrentDomain.BaseDirectory);
+                    File.WriteAllText("fake_dlc_temp\\fake_dlc_project.gp4", dir);
+                    Process process2 = new Process();
+                    process2.StartInfo.FileName = "cmd.exe";
+                    process2.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    string s = AppDomain.CurrentDomain.BaseDirectory + "fake_dlc_pkg";
+                    process2.StartInfo.Arguments = "/c data\\orbis-pub-cmd.exe img_create \"" + AppDomain.CurrentDomain.BaseDirectory + "fake_dlc_temp\\fake_dlc_project.gp4" + "\" \"" + AppDomain.CurrentDomain.BaseDirectory + "\\fake_dlc_pkg\\" + EPID + "-A0000-V0100.pkg";
+                    process2.Start();
+                    process2.WaitForExit();
+                    File.Delete("fake_dlc_temp\\param_template.sfx");
+                    File.Delete("fake_dlc_temp\\sce_sys\\param.sfo");
+                    File.Delete("fake_dlc_temp\\fake_dlc_project.gp4");
+                    Directory.Delete("fake_dlc_temp\\sce_sys");
+                    Directory.Delete("fake_dlc_temp");
                     progressBar1.Maximum = lboxFiles.Items.Count;
                     /// this is going to make the progress bar know how many files there are so it can move the bar acordingly
                     System.GC.Collect();
